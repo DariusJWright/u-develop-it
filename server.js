062a37117408f786e44849cf50737e0205d4ba4a
darshnablah <dwright758@gmail.com>
2020-08-05 18:59:13 -0500

    joined tables

--- server.js
+++ server.js
@@ -19,31 +19,40 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
 //Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                 AS party_name
+                 FROM candidates
+                 LEFT JOIN parties
+                 ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
             return;
         }
-        
+
         res.json({
             message: 'Success',
             data: rows
         });
     });
 });
-    
+
 // Get candidate by id
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+                 AS party_name 
+                 FROM candidates 
+                 LEFT JOIN parties 
+                 ON candidates.party_id = parties.id 
+                 WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
-        if(err) {
+        if (err) {
             res.status(500).json({ error: err.message });
             return;
         }
-        
+
         res.json({
             message: 'Success',
             data: row
@@ -55,7 +64,7 @@ app.get('/api/candidate/:id', (req, res) => {
 app.delete('/api/candidate/:id', (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`;
     const params = [req.params.id];
-    db.run(sql, params, function(err, result) {
+    db.run(sql, params, function (err, result) {
         if (err) {
             res.status(400).json({ error: err.message });
             return;
@@ -80,9 +89,9 @@ app.post('/api/candidate', ({ body }, res) => {
                     VALUES (?,?,?)`;
     const params = [body.first_name, body.last_name, body.industry_connected];
     // ES5 function, not arrow function, to use 'this'
-    db.run(sql, params, function(err, result) {
+    db.run(sql, params, function (err, result) {
         if (err) {
-            res.status(400).json ({ error: err.message });
+            res.status(400).json({ error: err.message });
             return;
         }
 
